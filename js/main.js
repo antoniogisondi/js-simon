46db0b9f30379b05eefcaed031ad7df27fcdb739
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-01 17:07:17 +0200

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,46 +1,45 @@
 // CREO LA FUNZIONE CHE MI GENERERA' I NUMERI RANDOM
-function randonNumber(max, min){
+function randonNumber(min, max){
     return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
-// CREO L'ARRAY CHE MI CONTERRA' I NUMERI RANDOM
-let number = []
 
 // CICLO LA FUNZIONE PER GENERARE 5 NUMERI RANDOM
-for( let i = 0; i < 5; i++){
-    number.push(randonNumber(0,100))
-    document.querySelector('.number').innerHTML = number
+for( let i = 1; i <= 5; i++){
+    let number = randonNumber(0,100)
+    console.log(number)
+    let box = document.querySelector('.number')
+    box.innerHTML = `<p>${number}</p>`
     let message = document.querySelector('.message')
     setTimeout(function(){
-        document.querySelector('.number').innerHTML = ''
-   },30000)
-
-// ESEGUO IL PROMPT
-   setTimeout(function(){
-    let promptUser;
-    do{
-        promptUser = parseInt(prompt('Inserisci i numeri che ricordi'))
-    }
-    while(isNaN(promptUser)){
-        console.log(promptUser)
-    }
-
-    if(number === promptUser){
-        document.querySelector('.number').innerHTML = number
-        message.innerHTML += `Il tuo risultato: ${number} è corretto`
-    }
-    else{
-        document.querySelector('.number').innerHTML = number
-        message.innerHTML += `Il tuo risultato: ${promptUser} non è corretto, la risposta giusta era ${number}`
-    }
-   },30020)
-}
+        box.innerHTML = ''
+    },5000)   
+    // ESEGUO IL PROMPT
+       setTimeout(function(){
+        let promptUser;
+        do{
+            promptUser = parseInt(prompt('Inserisci i numeri che ricordi'))
+        }
+        while(isNaN(promptUser)){
+            console.log(promptUser)
+        }
     
+        if(number == promptUser){
+            message.innerHTML += `Il tuo risultato: ${number} è corretto`
+        }
+        else{
+            message.innerHTML += `Il tuo risultato: ${promptUser} non è corretto`
+        }
+
+    },5020)
+}
 
 
-
-
-
-
-
-
+   
+   
+   
+   
+   
+   
+   
+   
